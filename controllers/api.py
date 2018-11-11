616b920aa8c016ff6c0affd3f849ed1fef1e5f59
Yu-Shuo Li <likevin111@gmail.com>
2018-11-11 15:24:35 -0800

    hw3 base

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,45 @@
 # Here go your api methods.
+
+
+@auth.requires_signature()
+def add_post():
+    post_id = db.post.insert(
+        post_title=request.vars.post_title,
+        post_content=request.vars.post_content,
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(post_id=post_id))
+
+
+def get_post_list():
+    results = []
+    if auth.user is None:
+        # Not logged in.
+        rows = db().select(db.post.ALL, orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                post_title=row.post_title,
+                post_content=row.post_content,
+                post_author=row.post_author,
+                thumb = None,
+            ))
+    else:
+        # Logged in.
+        rows = db().select(db.post.ALL, db.thumb.ALL,
+                            left=[
+                                db.thumb.on((db.thumb.post_id == db.post.id) & (db.thumb.user_email == auth.user.email)),
+                            ],
+                            orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.post.id,
+                post_title=row.post.post_title,
+                post_content=row.post.post_content,
+                post_author=row.post.post_author,
+                thumb = None if row.thumb.id is None else row.thumb.thumb_state,
+            ))
+    # For homogeneity, we always return a dictionary.
+    return response.json(dict(post_list=results))
+    
+
--- static/js/default_index.js
+++ static/js/default_index.js
@@ -1,5 +1,4 @@
 // This is the js for the default/index.html view.
-
 var app = function() {
 
     var self = {};
@@ -13,23 +12,103 @@ var app = function() {
         }
     };
 
+    // Enumerates an array.
+    var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
+
+    self.add_post = function () {
+        // We disable the button, to prevent double submission.
+        $.web2py.disableElement($("#add-post"));
+        var sent_title = self.vue.form_title; // Makes a copy 
+        var sent_content = self.vue.form_content; // 
+        $.post(add_post_url,
+            // Data we are sending.
+            {
+                post_title: self.vue.form_title,
+                post_content: self.vue.form_content
+            },
+            // What do we do when the post succeeds?
+            function (data) {
+                // Re-enable the button.
+                $.web2py.enableElement($("#add-post"));
+                // Clears the form.
+                self.vue.form_title = "";
+                self.vue.form_content = "";
+                // Adds the post to the list of posts. 
+                var new_post = {
+                    id: data.post_id,
+                    post_title: sent_title,
+                    post_content: sent_content
+                };
+                self.vue.post_list.unshift(new_post);
+                // We re-enumerate the array.
+                self.process_posts();
+            });
+        // If you put code here, it is run BEFORE the call comes back.
+    };
+
+    self.get_posts = function() {
+        $.getJSON(get_post_list_url,
+            function(data) {
+                // I am assuming here that the server gives me a nice list
+                // of posts, all ready for display.
+                self.vue.post_list = data.post_list;
+                // Post-processing.
+                self.process_posts();
+                console.log("I got my list");
+            }
+        );
+        console.log("I fired the get");
+    };
+
+    self.process_posts = function() {
+        // This function is used to post-process posts, after the list has been modified
+        // or after we have gotten new posts. 
+        // We add the _idx attribute to the posts. 
+        enumerate(self.vue.post_list);
+        // We initialize the smile status to match the like. 
+        self.vue.post_list.map(function (e) {
+            // I need to use Vue.set here, because I am adding a new watched attribute
+            // to an object.  See https://vuejs.org/v2/guide/list.html#Object-Change-Detection-Caveats
+            // The code below is commented out, as we don't have smiles any more. 
+            // Replace it with the appropriate code for thumbs. 
+            // // Did I like it? 
+            // // If I do e._smile = e.like, then Vue won't see the changes to e._smile . 
+            // Vue.set(e, '_smile', e.like); 
+        });
+    };
+
+
     // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            form_title: "",
+            form_content: "",
+            post_list: []
         },
         methods: {
+            add_post: self.add_post,
         }
 
     });
 
+    // If we are logged in, shows the form to add posts.
+    if (is_logged_in) {
+        $("#add_post").show();
+    }
+
+    // Gets the posts.
+    self.get_posts();
 
     return self;
 };
 
 var APP = null;
+
+// No, this would evaluate it too soon.
+// var APP = app();
 
 // This will make everything accessible from the js console;
 // for instance, self.x above would be accessible as APP.x
